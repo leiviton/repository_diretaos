d8dfd4c1a068c2984ac98324f2a3b3c17fd188a5
Leiviton Carlos <leivitoncs@gmail.com>
2016-11-16 11:29:30 -0200

    usuario de email

--- config/mail.php
+++ config/mail.php
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.direta.com.br'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'sistema@direta.com.br', 'name' => 'Sistema OS'],
 
     /*
     |--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('sistema@direta.com.br'),
 
     /*
     |--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return [
     |
     */
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('Dir2012'),
 
     /*
     |--------------------------------------------------------------------------
